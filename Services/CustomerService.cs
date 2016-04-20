46ecc3f8a49727afc9d94a61ce9e2c02abbe101a
Antonio-perez-alvarez <antoservoi23@gmail.com>
2016-04-20 15:20:19 +0200

    serviciossssssssssssssss

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -8,5 +8,19 @@ namespace Services
 {
     public class CustomerService : IService
     {
+        readonly IService _customer;
+        public CustomerService(IService customer)
+        {
+            if (null == customer)
+            {
+                throw new ArgumentNullException("The field customer can not be a \"null\"");
+            }
+            _customer = customer;
+        }
+        public void Add()
+        {
+            _customer.Add();
+        }
+     
     }
 }
