cb213475d917077c99e00bcfe0e6c0e3cc9e1da7
Antonio-perez-alvarez <antoservoi23@gmail.com>
2016-04-21 17:22:18 +0200

    Changes on Infrastructure

--- SilverGuacamole/Controllers/IUserContext.cs
+++ SilverGuacamole/Controllers/IUserContext.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Infraestructure
+namespace SilverGuacamole.Controllers
 {
-    class AppContext
+    public interface IUserContext
     {
-
     }
 }
