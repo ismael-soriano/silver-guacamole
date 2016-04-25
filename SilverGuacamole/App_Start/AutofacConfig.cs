4961a81dcd33599d7172873ca751b0a1824c3c85
niliana <niliana.carrero@gmail.com>
2016-04-25 13:53:05 +0200

    changes autofac

---
+++ SilverGuacamole/App_Start/AutofacConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+using Autofac.Integration.Mvc;
+using System.Web.Mvc;
+using SilveGuacamole.Controllers;
+using Services;
+
+
+
+namespace SilverGuacamole.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register dependencies in controllers
+            builder.RegisterControllers(typeof(SilverGuacamole.MvcApplication).Assembly);
+
+            builder.RegisterModelBinders(typeof(SilverGuacamole.MvcApplication).Assembly);
+            // Register our Data dependencies
+            builder.RegisterModule<AutofacWebTypesModule>();
+
+            builder.RegisterType<CustomerController>().As<Controller>().InstancePerHttpRequest();
+
+            builder.RegisterType<CustomerService>().As<ICustomerService>().InstancePerHttpRequest();
+
+            builder.RegisterType<CustomerService>().As<ICustomerService>().InstancePerHttpRequest();
+
+            var container = builder.Build();
+
+            // Set MVC DI resolver to use our Autofac container
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+
+    }
+}
--- SilverGuacamole/Global.asax.cs
+++ SilverGuacamole/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using SilverGuacamole.App_Start;
 
 namespace SilverGuacamole
 {
@@ -16,8 +18,8 @@ namespace SilverGuacamole
     {
         protected void Application_Start()
         {
+            AutofacConfig.ConfigureContainer();
             AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
