d72791387121fc16c806802ef14662571b30bdeb
ismael-soriano <luzaoban@gmail.com>
2016-04-26 09:09:50 +0200

    Config .gitignore

---
+++ SilverGuacamoleAPI/App_Start/AutofacConfig.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SilverGuacamoleAPI.Controllers;
+using Services;
+using Infraestructure;
+
+namespace SilverGuacamoleAPI.App_Start
+{
+    public class AutofacConfig
+    {
+        public static IContainer ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register dependencies in controllers
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterModelBinders(typeof(MvcApplication).Assembly);
+            builder.RegisterModule<AutofacWebTypesModule>();
+            builder.RegisterType<CustomerController>().As<Controller>().InstancePerHttpRequest();
+            builder.RegisterType<CustomerService>().As<ICustomerService>().InstancePerHttpRequest();
+            builder.RegisterType<AppContext>().As<IRepositoryCustomer>().InstancePerHttpRequest();
+            builder.RegisterModelBinderProvider();
+            return builder.Build();
+        }
+    }
+}
---
+++ SilverGuacamoleAPI/Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SilverGuacamoleAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        readonly ICustomerService _service;
+        public CustomerController(ICustomerService service)
+        {
+            if (null == service)
+            {
+                throw new ArgumentNullException("service");
+            }
+
+            _service = service;
+        }
+
+        // GET api/customer
+        public IEnumerable<string> Get()
+        {
+            return _service.GetAll("") as IEnumerable<string>;
+
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/customer/5
+        public string Get(int id)
+        {
+            return _service.Get(id).ToString();
+            return "value";
+        }
+
+        // POST api/customer
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/customer/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/customer/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
